b7ddd90b23bde006fb2bc18303b833c8c2553e74
kmanthrige <k.manthrige@gmail.com>
2023-05-02 14:43:06 -0500

    started the assignemnt

---
+++ assignement.js
@@ -0,0 +1,12 @@
+console.log('***** Music Collection *****')
+
+let collection = []
+// let title = 'Endless Summer Vacation'
+// let artist = 'Miley Cyrus'
+// let yearPublished = 2023
+
+function addToCollection(title, artist, yearPublished) {
+    this.title = title
+    this.artist = artist
+    this.yearPublished = yearPublished
+}
