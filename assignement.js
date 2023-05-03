4cb0c62ed522105692735408e607701430cb50e3
kmanthrige <k.manthrige@gmail.com>
2023-05-03 11:29:59 -0500

    required assignments complete

--- assignement.js
+++ assignement.js
@@ -5,13 +5,13 @@ let collection = []
 
 function addToCollection(title, artist, yearPublished) {
     let myCollection = {
-    title: title,
-    artist: artist,
-    yearPublished: yearPublished
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
     }
     collection.push(myCollection)
     return collection
-    
+
 }
 
 addToCollection('esm', 'mc', 2023)
@@ -22,3 +22,30 @@ addToCollection('hwwiw', 'sg', 2017)
 
 console.log('collection:', collection)
 
+function showCollection() {
+    console.log('number of items in the array', collection.length)
+    for (i = 0; i < collection.length; i++) {
+        console.log(collection[i].title, 'by', collection[i].artist + ', published in', collection[i].yearPublished)
+    }
+}
+
+showCollection()
+
+let songsByArtist = []
+
+function findByArtist(artist) {
+    for (i = 0; i < collection.length; i++) {
+        if (collection[i].artist === artist) {
+            console.log('in if:', songsByArtist.push(collection[i]))
+        }
+    }//end for loop
+    return songsByArtist
+}//end findByArtist function
+
+findByArtist('sg')
+
+console.log('Find by artist:', songsByArtist)
+
+function search(){
+ 
+}
