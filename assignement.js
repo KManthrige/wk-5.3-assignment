801674b05191aa2f9039bfe7cb1ab75b1ab62078
kmanthrige <k.manthrige@gmail.com>
2023-05-03 17:53:12 -0500

    stretch goals halfway complete

--- assignement.js
+++ assignement.js
@@ -3,22 +3,30 @@ console.log('***** Music Collection *****')
 let collection = []
 
 
-function addToCollection(title, artist, yearPublished) {
+function addToCollection(title, songs, artist, yearPublished) {
     let myCollection = {
-        title: title,
-        artist: artist,
-        yearPublished: yearPublished
+        title,
+        songs,
+        artist,
+        yearPublished
     }
     collection.push(myCollection)
     return collection
 
 }
 
-addToCollection('esm', 'mc', 2023)
-addToCollection('hmbomt', 'bs', 1998)
-addToCollection('ml', 'wl', 2003)
-addToCollection('wb', 'mc', 2017)
-addToCollection('hwwiw', 'sg', 2017)
+addToCollection(
+    'esm',
+    [{ name: 'song1', duration: 3.09 }, { name: 'song2', duration: 5.23 }],
+    'mc',
+    2023
+)
+addToCollection(
+    'hmbomt',
+    [{ name: 'track1', duration: 2.01 }, { name: 'track2', duration: 1.02 }],
+    'bs',
+    1998
+)
 
 console.log('collection:', collection)
 
@@ -39,13 +47,51 @@ function findByArtist(artist) {
             console.log('in if:', songsByArtist.push(collection[i]))
         }
     }//end for loop
-    return songsByArtist
+    return artist
 }//end findByArtist function
 
-findByArtist('sg')
+findByArtist('mco')
 
 console.log('Find by artist:', songsByArtist)
 
-function search(){
- 
+
+let searchItem = {
+    artist: '',
+    songs: '',
+    year: 2007
 }
+let foundItems = []
+
+function search(criteria) {
+    if (criteria.artist === '' && criteria.songs === '' && criteria.year === null) {
+        console.log('first if statement')
+        return collection
+    }//end if checking is there is any search criteria in the object
+    if (criteria.songs) {
+        console.log('second if statement')
+        for (i = 0; i < collection.length; i++) {
+            for (t = 0; t < collection[i].songs.length; t++) {
+                if (collection[i].songs[t].name === criteria.songs) {
+                    foundItems.push(collection[i])
+                }//end if statement looking for criteria.songs
+            }//end for loop 2
+        }//end for loop 1
+    }
+    if ((criteria.artist || criteria.year) && criteria.songs === '') {
+        console.log('third if statement')
+        for (p = 0; p < collection.length; p++) {
+            if (collection[p].artist === criteria.artist || collection[p].yearPublished === criteria.year) {
+                foundItems.push(collection[p])
+            }
+        }//end for loop for artist and year
+    }//end if for artist and year
+
+    return foundItems
+}//end function search
+
+console.log(search(searchItem))
+
+
+//add an array of tracks to collection{}. the tracks[] should have tracks[ name, duration]. 
+//update the functions to support tracks
+//update addToCollection 
